1edda8e0e741a32d499f231f715d21e957c4d84a
luttinenjoni <luttinenjoni@gmail.com>
2024-12-20 13:13:20 +0200

    made changes to database and added database data to the endpoints.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { pgPool } from './pg_connection.js'
+import { json } from 'stream/consumers'
 
 const app = express()
 
@@ -7,19 +8,43 @@ app.listen(3001, () => {
     console.log("Server is running now!")
 })
 
+try{
+    console.log('Database connected')
+       
+}catch(e){
+    console.log(e.message); 
+}
 
-app.get("/movies", (req, res) => {
-    res.send('movies here')
+app.get("/movies", async(req, res) => {
+    try{
+        const result =  await pgPool.query('SELECT * FROM movies');
+        res.json(result.rows);
+           
+    }catch(e){
+        res.status(400).json({e: e.message});
+    }
 })
 
 app.get("/movie_review", (req, res) => {
     res.send('reviews here')
 })
 
-app.get("/genres", (req, res) => {
-    res.send('genres here')
+app.get("/genres", async(req, res) => {
+    try{
+        const result =  await pgPool.query('SELECT * FROM genres');
+        res.json(result.rows);
+           
+    }catch(e){
+        res.status(400).json({e: e.message});
+    }
 })
 
-app.get("/users", (req, res) => {
-    res.send('users here')
+app.get("/users", async (req, res) => {
+    try{
+        const result =  await pgPool.query('SELECT * FROM users');
+        res.json(result.rows);
+           
+    }catch(e){
+        res.status(400).json({e: e.message});
+    }
 })
